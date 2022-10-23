cee7f31b2bacb45488fef623c8b7663fc506f5c4
Romulo Negrillo Jr <romnegrillo@gmail.com>
2022-10-23 19:21:09 +0800

    feat: Added login google auth and modifed login component.

--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -1,21 +1,62 @@
-import { React } from "react"
-import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+import React from "react";
+
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import jwt_decode from "jwt-decode";
+import { useNavigate } from "react-router-dom";
+
+import shareVideo from "../assets/share.mp4";
+import logo from "../assets/logowhite.png";
+
+import { client } from "../client";
 
 const Login = () => {
-    const googleResponse = (credentialResponse) => {
-        console.log(credentialResponse)
-    }
+  const navigate = useNavigate();
+  const responseGoogle = (response) => {
+    const decoded = jwt_decode(response.credential);
+    const { name, sub, picture } = decoded;
 
-    return (
-        <div>
-            <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
-                <GoogleLogin
-                    onSuccess={googleResponse}
-                    onError={googleResponse}
-                />;
-            </GoogleOAuthProvider>
+    const doc = {
+      _id: sub,
+      _type: "user",
+      userName: name,
+      image: picture,
+    };
+
+    client.createIfNotExists(doc).then(() => {
+      navigate("/", { replace: true });
+    });
+
+  };
+
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="relative w-full h-full">
+        <video
+          src={shareVideo}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
+      </div>
+
+      <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+        <div className="p-5">
+          <img src={logo} alt="logo" />
         </div>
-    )
-}
+
+        <div className="shadow-2x1">
+          <GoogleOAuthProvider
+            clientId={import.meta.env.VITE_REACT_APP_GOOGLE_API_TOKEN}
+          >
+            <GoogleLogin onSuccess={responseGoogle} onError={responseGoogle} />
+          </GoogleOAuthProvider>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
